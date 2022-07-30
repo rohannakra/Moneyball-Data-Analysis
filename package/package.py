327642ed2dfe7dca67ee0681ce8f5399fbb8a4ed
RoJuan230 <rohan.nakra100@gmail.com>
2022-07-29 23:13:08 -0700

    Organizing more.

---
+++ package/package.py
@@ -0,0 +1,20 @@
+# Import sklearn/tensorflow modules.
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, f1_score
+
+# Import other modules.
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pickle
+import os
+from time import sleep
+from sportsipy.mlb.teams import Teams
+from IPython.display import clear_output
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+dataset = pd.read_csv('data management/data_all.csv')
+
+display(dataset)
